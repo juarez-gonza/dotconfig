3edadd21859f99301a5011398b1ada1c5e2dae77
gonzalo juare <gonzalo@montyBSD.vm>
2021-03-12 02:06:07 -0300

    Fri Mar 12 02:06:07 -03 2021

--- dwm-6.2/config.def.h
+++ dwm-6.2/config.def.h
@@ -6,6 +6,9 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 12;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 
+static const int vertpad            = 0;       /* vertical padding of bar */
+static const int sidepad            = 0;       /* horizontal padding of bar */
+
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
